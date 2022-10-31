ba677262d385c72e1a09d344c3656c4285620ab5
User <User@DESKTOP-ESP2JT1>
2022-10-31 15:13:38 +0200

    Add project files.

---
+++ 04. Inches to Centimeters/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _04._Inches_to_Centimeters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double santimeter = double.Parse(Console.ReadLine());
+            
+                   
+            Console.WriteLine(santimeter * 2.54);
+           
+        }
+    }
+}
